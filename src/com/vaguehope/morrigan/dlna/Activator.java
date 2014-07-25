c909c981d10d4849629ba82a5bc853adbf677629
haku <git@haku.me>
2014-07-25 20:49:05 +0100

    Logback.

--- src/com/vaguehope/morrigan/dlna/Activator.java
+++ src/com/vaguehope/morrigan/dlna/Activator.java
@@ -5,12 +5,17 @@ import java.util.concurrent.ScheduledExecutorService;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.teleal.cling.UpnpService;
 import org.teleal.cling.UpnpServiceImpl;
 
 import com.vaguehope.morrigan.dlna.httpserver.MediaServer;
+import com.vaguehope.morrigan.dlna.util.LogHelper;
 
 public class Activator implements BundleActivator {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Activator.class);
 
 	private MediaServer mediaServer;
 	private PlayerHolder playerHolder;
@@ -20,6 +25,8 @@ public class Activator implements BundleActivator {
 
 	@Override
 	public void start (final BundleContext context) {
+		LogHelper.bridgeJul();
+
 		this.scheduledExecutor = Executors.newScheduledThreadPool(1);
 
 		this.mediaServer = new MediaServer();
@@ -32,17 +39,37 @@ public class Activator implements BundleActivator {
 		this.upnpService.getRegistry().addListener(new DeviceWatcher(this.playerRegisterListener));
 		this.upnpService.getControlPoint().search();
 
-		System.err.println("DLNA started.");
+		LOG.info("DLNA started.");
 	}
 
 	@Override
 	public void stop (final BundleContext context) {
-		this.scheduledExecutor.shutdownNow();
-		this.playerRegisterListener.dispose();
-		this.playerHolder.dispose();
-		this.upnpService.shutdown();
-		this.mediaServer.dispose();
-		System.err.println("DLNA stopped.");
+		if (this.scheduledExecutor != null) {
+			this.scheduledExecutor.shutdownNow();
+			this.scheduledExecutor = null;
+		}
+
+		if (this.playerRegisterListener != null) {
+			this.playerRegisterListener.dispose();
+			this.playerRegisterListener = null;
+		}
+
+		if (this.playerHolder != null) {
+			this.playerHolder.dispose();
+			this.playerHolder = null;
+		}
+
+		if (this.upnpService != null) {
+			this.upnpService.shutdown();
+			this.upnpService = null;
+		}
+
+		if (this.mediaServer != null) {
+			this.mediaServer.dispose();
+			this.mediaServer = null;
+		}
+
+		LOG.info("DLNA stopped.");
 	}
 
 }
