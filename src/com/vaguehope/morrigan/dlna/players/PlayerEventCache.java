83b3d7d50aa8abcb38f69c1b32bb2cf646dc32a5
haku <git@haku.me>
2017-01-09 17:08:02 +0000

    Transcode is now in own bundle.

--- src/com/vaguehope/morrigan/dlna/players/PlayerEventCache.java
+++ src/com/vaguehope/morrigan/dlna/players/PlayerEventCache.java
@@ -7,7 +7,7 @@ import com.vaguehope.morrigan.engines.playback.IPlaybackEngine.PlayState;
 import com.vaguehope.morrigan.player.OrderHelper.PlaybackOrder;
 import com.vaguehope.morrigan.player.PlayItem;
 import com.vaguehope.morrigan.player.Player.PlayerEventListener;
-import com.vaguehope.morrigan.player.transcode.Transcode;
+import com.vaguehope.morrigan.transcode.Transcode;
 
 public class PlayerEventCache implements PlayerEventListener {
 
